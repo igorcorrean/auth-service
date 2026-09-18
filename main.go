060b27d3f8c10ebe5fdc42ed0b429e9163f3b9ca
igorcorrean <igorconunes@gmail.com>
2026-09-17 23:08:34 -0300

    Teste para validacao do devsecops

--- main.go
+++ main.go
@@ -58,7 +58,7 @@ func main() {
 		driver = "postgres"
 	}
 
-	masterKey := os.Getenv("MASTER_KEY")
+	masterKey = "SenhaSuperInsegura"
 	if masterKey == "" {
 		log.Fatal("Erro crítico: A variável de ambiente MASTER_KEY está vazia.")
 	}
